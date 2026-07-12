aa79412de5745d0ba9e21dabee106c3496c63e68
Ethan Haque <ethan.k.haque@gmail.com>
2026-07-12 05:29:33 -0400

    fix: remove message at beginning of init

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
-if vim.fn.has("nvim-0.12") == 0 then
-    error("This configuration requires Neovim 0.12 or newer")
-end
-
 require("config.options")
 require("config.packages")
 require("config.lsp")
